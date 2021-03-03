0916e730d2de145c590afb601ad01a681b37fc01
Sy Rashid <sy.rashid.11@gmail.com>
2021-03-03 04:30:38 -0500

    FIX: Routing mini lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
   resources :friends
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
+# Option 1
+# Non CRUD way, NOT RESTFUL
+get '/requests', to: 'bookings#requests' # Offers made on my developer or artwork, on what other people have tried to book on your items, add functionality to either accept or reject
+
+
+# get '/bookings', to: 'bookings#index'  bookings are on what you've tried to book,
+resources :bookings, only: :index  # Bookings that I have made, maybe add a review functionality
+
+# Option 2
+# CRUD Way, RESTFUL
+# DashboardsController would have 1 action, show
+# dashboard, to: 'dashboards#show'
+resource :dashboard, only: :show
+
 end
